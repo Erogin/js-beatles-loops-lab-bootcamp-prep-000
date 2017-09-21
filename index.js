fc0c771c83fbd187b6f2efb25953175d87c12adc
Eric Roginek <eroginek@protonmail.com>
2017-09-21 15:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 function theBeatlesPlay(musicians, instruments){
-  var variable = {}
-    for (let i = 0; i <= musicians.length; i++){
-      var variable = {musicians + " plays " + instruments}
-      variable.push()
+  var sentenceObj = []
+    for (let i = 0; i < musicians.length; i++){
+      var sentence = musicians[i] + " plays " + instruments[i]
+      sentenceObj[i] = sentence
     }
-    return variable
+    return sentenceObj
+}
+
+function johnLennonFacts(facts){
+  let i = 0
+  var length = facts.length
+  while (i< length){
+    facts[i] = facts[i] +"!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var emptArray = []
+  let i = 0
+    do {
+    console.log('I love the Beatles!')
+    i++
+  } while (i<15)  
 }
